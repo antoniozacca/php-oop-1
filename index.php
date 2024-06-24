259f6c57490ba5f5617c37d178045d390fafdabb
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-24 16:18:40 +0200

    add Exception

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 class Movie {
    private $nome;
    private $genere;
+   private $voto;
    function setNome($_nome){
       $this->nome=$_nome;
    }
@@ -14,10 +15,35 @@ class Movie {
    function getGenere(){
       return $this->genere;
    }
+   function setVoto($_voto){
+      if($_voto < 0 || $_voto > 5){
+         throw new Exception('Inserisci un voto compreso tra 0 e 5!!!!!');
+      }
+
+      $this->voto=$_voto;
+   }
+   function getVoto(){
+      return $this->voto;
+   }
+   // function controllaVoto(){
+   //    if($this->voto < 0 || $this->voto > 5){
+   //       throw new Exception('Inserisci un voto compreso tra 0 e 5!!!!!');
+   //    }
+   //    return $this->voto;
+   // }
+   
 }
+
 $film = new Movie();
 $film->setNome('the wolf of wall street');
 echo $film->getNome();
 echo "<br>";
 $film->setGenere('commedia');
 echo $film->getGenere();
+echo "<br>";
+try {
+   $film->setVoto(1);
+   echo $film->getVoto();
+} catch (Exception $e) {
+   echo $e->getMessage();
+}
