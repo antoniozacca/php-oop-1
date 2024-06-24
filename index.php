1f0514625bfd2f63602cff4ed6c87bdde3ee66fa
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-24 14:44:45 +0200

    add metodo

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 class Movie {
-   public $nome;
-   public $genere;
+   private $nome;
+   private $genere;
    function __construct($_nome)
    {
     $this->nome=$_nome;
    }
+   function getNome(){
+      return $this->nome;
+   }
+   function setGenere($_genere){
+      $this->genere=$_genere;
+   }
+   function getGenere(){
+      return $this->genere;
+   }
 }
 $film= new Movie('the wolf of wall street');
-echo $film->nome;
+echo $film->getNome();
+$film->setGenere('commedia');
+echo $film->getGenere();
